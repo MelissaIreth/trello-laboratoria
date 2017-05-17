9d578f2e4e8f06363c3a5acd6198cc551a661e19
Melissa <melissa.pacheco.martinez@gmail.com>
2017-05-17 15:29:03 -0400

    avance

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,18 @@
-/*function agregar(){
-	var anadir = document.getElementByClassName("anadir");
-	anadir.addEventListener("click"function(){
-		document.createElement("textarea")
-	})
-
-	var nuevasTareas = document.createElement("div");
-	var textoNuevaTarea = document.createTextNode(tareas);
-	var elementoContenedor = document.createElement("span");
-
-	elementoContenedor.appendChild(textoNuevaTarea);
-	nuevasTareas.appendChild(elementoContenedor);
-	agregar.appendChild(nuevasTareas);
-
-	var borrar = document.createElement("span");
-	borrar.classList.add("fa","fa-trash-o");
-
-	nuevasTareas.appendChild(borrar);
-
-	borrar.addEventListener("click", function(){
-		agregar.removeChild(nuevasTareas);
-	})
-}
-*/
+	var lista = document.getElementsByClassName("lista")[0];
+	var a = document.createElement("a");
+	a.setAttribute("class","la-a");
+	var text = document.createTextNode("Añadir una lista");
+	a.appendChild(text);
+	lista.appendChild(a);
 
 function agregar(){
-	var anadir = document.getElementByClassName("tareas");
-	var a = document.createElement("a");
-	a.setAtribute("class","la-a")
-	var text = document.createElement("textarea")
-	text.setAtribute("class","el-texto")
-	anadir.appendChild(a)
-	a.appendChild(text)
-	anadir.addEventListener("click", text)
-}
-agregar();
+	var agregar = document.getElementsByClassName("la-a")[0];
+	var cuadroTexto = document.createElement("textarea");
+	var espan = document.createElement("span")
+	cuadroTexto.appendChild(espan)
+	cuadroTexto.setAttribute("class","el-texto");
+	lista.addEventListener("click", function(){
+		cuadroTexto;
+	});
+};
+
